00e2b5cf5238b8e43daeca3c8caa856ceee8e2b1
Samirborges <alessandro.samir@souunit.com.br>
2026-07-04 14:59:29 -0300

    feat(users): add user registration serializer

---
+++ backend/users/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    confirm_password = serializers.CharField(
+        write_only=True
+    )
+    
+    class Meta:
+        
+        model = User
+        
+        fields = (
+            "username",
+            "email",
+            "password",
+            "confirm_password"
+        )
+        
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
+        
+    
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                {
+                    "confirm_password": "Password do not match."
+                }
+            )    
+            
+        return attrs
+        
+        
+    def create(self, validated_data):
+        """
+        Creates a new user using the create_user method,
+        ensuring that the password is stored securely.
+        """
+        
+        validated_data.pop("confirm_password")
+        
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
+        )
+        
+        return user
